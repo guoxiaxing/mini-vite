7e2907cb051ac809b583c28f9b5b40b289abce9b
guoxx03 <guoxx03@yuanfudao.com>
2022-12-12 17:54:32 +0800

    FEAT: mini-vite 实现

---
+++ src/node/plugins/index.ts
@@ -0,0 +1,15 @@
+import { Plugin } from "../plugin";
+import { esbuildTransformPlugin } from "./esbuild";
+import { importAnalysisPlugin } from "./importAnalysis";
+import { resolvePlugin } from "./resolve";
+import { cssPlugin } from "./css";
+import { assetPlugin } from "./assets";
+import { clientInjectPlugin } from './clientInject';
+
+
+export function resolvePlugins(): Plugin[] {
+    // 下一部分会逐个补充插件逻辑
+    // clientInject插件最好放到最前面的位置，以免后续插件的 load 钩子干扰客户端脚本的加载。
+    return [clientInjectPlugin(), resolvePlugin(), esbuildTransformPlugin(), importAnalysisPlugin(), cssPlugin(), assetPlugin()];
+  }
+  
